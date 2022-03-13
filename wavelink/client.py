ae6c0f24b39b3896b184d53ded1b5636c7385599
Lonely Guy <phoenex659@gmail.com>
2022-03-13 15:50:16 +0530

    Update client.py

--- wavelink/client.py
+++ wavelink/client.py
@@ -23,7 +23,8 @@ SOFTWARE.
 import aiohttp
 import asyncio
 import logging
-from disnake.ext import commands
+import discord as disnake
+from discord.ext import commands
 from functools import partial
 from json import dumps, loads
 from json.decoder import JSONDecodeError
